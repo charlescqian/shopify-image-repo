9f2c7d5c4a87bddf224a6ba8d89c61561decd248
charlescqian <charlesqian.ubc@gmail.com>
2021-05-05 23:10:46 -0700

    Added endpoint for recent images, currently just fetches all images in the DB'

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 5000;
+let path = require("path");
 
 const dbName = "images";
 const colName = "images";
@@ -12,16 +13,30 @@ MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(client => {
         console.log("Successfully connected to Database")
         const collection = client.db(dbName).collection(colName);
+        console.log(path.join(__dirname, '../frontend/build/index.html'));
 
+        app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+        app.get('/', (req, res) => {
+            res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+        })
+        /*
+         * Get recent images uploaded by all users
+         *
+         */
         app.get('/api/images/recent', (req, res) => {
-
+            console.log('API Called');
             collection.find({}).toArray()
                 .then(result => {
-                    console.log(result)
-                    res.send(result)
+                    console.log(result);
+                    res.send(result);
                 })
                 .catch(error => console.error(error))
         })
+        
 
+        app.listen(port, () =>
+            console.log(`Server listening on port ${port}`)
+        )
     })
     .catch(error => console.error(error))
