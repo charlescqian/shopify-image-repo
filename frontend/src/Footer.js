cd854d32572136ebc2b43d67e01f7a364ddf7487
charlescqian <charlesqian.ubc@gmail.com>
2021-05-08 16:17:19 -0700

    Added Github and Linkedin Icons to Footer

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 import { withStyles } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Grid from '@material-ui/core/Grid';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import IconButton from '@material-ui/core/IconButton';
 
 const styles = theme => ({
     footer: {
@@ -30,6 +34,14 @@ class Footer extends Component {
                     <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
                         Charles Qian
                     </Typography>
+                    <Grid container justify="center">
+                        <IconButton href="https://github.com/charlescqian/shopify-image-repo" target="_blank">
+                            <GitHubIcon />
+                        </IconButton>
+                        <IconButton href="https://www.linkedin.com/in/charlescqian/" target="_blank">
+                            <LinkedInIcon/>
+                        </IconButton>
+                    </Grid>
                     <Copyright />
                 </footer>
             </div>
